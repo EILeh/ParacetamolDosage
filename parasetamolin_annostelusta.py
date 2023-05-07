e85e4138dfee48fede5e52c823d744bd6b536e24
Elli Lehtimäki <elli.i.lehtimaki@gmail.com>
2023-05-07 18:21:49 +0300

    Description added

--- parasetamolin_annostelusta.py
+++ parasetamolin_annostelusta.py
@@ -1,5 +1,11 @@
 """
 Paracetamol Dosage
+
+Counts the amount of paracetamol that user can take. Asks the user
+to five the person's weight, how much time has passed since the
+previous doze and the total doze in last 24 hours. The max amount
+that can be taken in 24 hours is 4 000 mg.
+
 Writer of the program: EILeh
 """
 
